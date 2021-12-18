1ade1a01a0aed713c1275823b118d20e378732fc
Anoopk2021 <anoop.k.2017@gmail.com>
2021-12-18 22:47:44 +0530

    Swap Two Numbers Without Using Third Variable

---
+++ src/SwapTwoNumbersWithoutUsingThirdVariable.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SwapTwoNumbersWithoutUsingThirdVariable {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter the Value of X and Y ");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println("Before swapping Numbers: " + x + " " + y);
+        x = x+y;
+        y = x-y;
+        x = x-y;
+        System.out.println("After Swapping: " + x + " " + y);
+
+    }
+}
