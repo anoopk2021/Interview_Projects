eeeddfc44c87c9115d69396de8a7ec8612a270c5
Anoopk2021 <anoop.k.2017@gmail.com>
2021-12-27 10:41:25 +0530

    ArraylistSorting

---
+++ src/ArraylistSorting.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class ArraylistSorting {
+
+    static class Main {
+
+        public static void main(String[] args) {
+
+            // create an ArrayList
+            ArrayList<String> languages = new ArrayList<>();
+
+            // add elements to ArrayList
+            languages.add("Python");
+            languages.add("Swift");
+            languages.add("C");
+            languages.add("JavaScript");
+            System.out.println("Unsorted ArrayList: " + languages);
+
+            // sort the ArrayList in ascending order
+            languages.sort(Comparator.naturalOrder());
+
+            System.out.println("Sorted ArrayList: " + languages);
+        }
+    }
+}
