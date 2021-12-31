31a643d8ec03d159694e93eec691f07b6defd789
Anoopk2021 <anoop.k.2017@gmail.com>
2021-12-31 12:53:01 +0530

    Remove_Duplicates_From_ArrayList

---
+++ src/main/Remove_Duplicates_From_ArrayList.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Remove_Duplicates_From_ArrayList {
+
+        public static void main(String[] args) {
+
+            // create an arraylist from the array
+            // using asList() method of the Arrays class
+            ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 1, 3));
+            System.out.println("ArrayList with duplicate elements: " + numbers);
+
+            // convert the arraylist into a set
+            Set<Integer> set = new LinkedHashSet<>();
+            set.addAll(numbers);
+
+            // delete al elements of arraylist
+            numbers.clear();
+
+            // add element from set to arraylist
+            numbers.addAll(set);
+            System.out.println("ArrayList without duplicate elements: " + numbers);
+        }
+    }
