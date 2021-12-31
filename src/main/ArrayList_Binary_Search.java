8321f0d00b61929c6b0f14625e6a5aa461196135
Anoopk2021 <anoop.k.2017@gmail.com>
2021-12-31 13:02:33 +0530

    ArrayList_Binary_Search

---
+++ src/main/ArrayList_Binary_Search.java
@@ -0,0 +1,37 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayList_Binary_Search {
+
+
+        public static void main(String[] args) {
+
+            ArrayList<Integer> aListNumbers
+                    = new ArrayList<Integer>();
+
+            aListNumbers.add(7);
+            aListNumbers.add(2);
+            aListNumbers.add(9);
+            aListNumbers.add(5);
+            aListNumbers.add(8);
+
+            System.out.println("Original ArrayList: " + aListNumbers);
+
+            //ArrayList must be sorted before binary search
+            Collections.sort(aListNumbers);
+
+            System.out.println("Sorted ArrayList: " + aListNumbers);
+
+            //binary search ArrayList
+            int index = Collections.binarySearch(aListNumbers, 8);
+
+            if(index >= 0)
+                System.out.println("Element found at index: " + index);
+            else
+                System.out.println("Insertion point: " + index);
+
+        }
+
+}
