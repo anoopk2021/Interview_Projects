d762c67964397bd1fea0bb8e7c3c9b2a6f8f7ac7
Anoopk2021 <anoop.k.2017@gmail.com>
2023-07-19 00:21:48 +0530

    Find Highest Value Using MaxBy

---
+++ src/main/FindHighestValueUsingMaxBy.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FindHighestValueUsingMaxBy {
+    public static void main(String[] args) {
+      List<Integer> items = Arrays.asList(25000, 45000, 70000, 90000, 35000);
+        Integer highestSalary = items.stream()
+                .collect(Collectors.maxBy((x, y) -> x - y)).get();
+        System.out.println(highestSalary);
+    }
+}
