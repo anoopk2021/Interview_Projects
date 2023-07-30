14a6a03c15e90ea69355c0dc24482269b6f9d2ee
anoopk2021 <anoop.k.2017@gmail.com>
2023-07-30 12:51:27 +0530

    Counting_Repeated_Numbers_Java8

--- src/coding_with_java8/Counting_Repeated_Numbers.java
+++ src/coding_with_java8/Counting_Repeated_Numbers.java
@@ -1,25 +1,20 @@
-package interview_coding_solution;
+package coding_with_java8;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
-public class Counting_Repeated_Numbers_Java8 {
+public class Counting_Repeated_Numbers {
     public static void main(String[] args) {
-
-
         ///11,12,32,43,11,23,12,32,43,43,
         //Requirement - Counting Repeated Numbers
-
-
         List<Integer> numberas = Arrays.asList(11, 122, 11, 23, 12, 11, 34, 23);
 
-        Map<Integer, Long> collect = numberas.stream().collect(Collectors.groupingBy(Integer::intValue, Collectors.counting()));
+        Map<Integer, Long> collect = numberas.stream().collect(Collectors.groupingBy
+                (Integer::intValue, Collectors.counting()));{
 
+            System.out.println(collect);
 
-        System.out.println(collect);
-
-
+        }
     }
 }
