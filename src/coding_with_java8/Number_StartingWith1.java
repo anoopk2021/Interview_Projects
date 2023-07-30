e7788303a3a787e2752b49dc5208e10561e6f366
anoopk2021 <anoop.k.2017@gmail.com>
2023-07-30 17:21:44 +0530

    Number_StartingWith1

---
+++ src/coding_with_java8/Number_StartingWith1.java
@@ -0,0 +1,14 @@
+package coding_with_java8;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Number_StartingWith1 {
+    public static void main(String[] args) {
+        List<Integer> myList = Arrays.asList(10,15,8,49,25,98,32);
+        myList.stream()
+                .map(s -> s + "") // Convert integer to String
+                .filter(s -> s.startsWith("1"))
+                .forEach(System.out::println);
+    }
+}
