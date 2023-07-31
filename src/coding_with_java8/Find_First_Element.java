a6928593faace8f522230fce6c952adaa54638c2
anoopk2021 <anoop.k.2017@gmail.com>
2023-07-31 23:06:24 +0530

    Find_First_Element

---
+++ src/coding_with_java8/Find_First_Element.java
@@ -0,0 +1,11 @@
+package coding_with_java8;
+import java.util.Arrays;
+import java.util.List;
+public class Find_First_Element {
+    public static void main(String[] args) {
+        List<Integer> myList = Arrays.asList(10,15,8,49,25,98,98,32,15);
+        myList.stream()
+                .findFirst()
+                .ifPresent(System.out::println);
+    }
+}
