a75afbf7622946aca6263345148d41ed9ef7e60d
anoopk2021 <anoop.k.2017@gmail.com>
2023-09-01 00:15:29 +0530

    SortValuesInAscendingOrder

--- src/coding_with_java8/SortValuesInAscendingOrder.java
+++ src/coding_with_java8/SortValuesInAscendingOrder.java
@@ -1,9 +1,7 @@
 package coding_with_java8;
 import java.util.*;
-import java.util.stream.*;
-import java.util.function.Function;
 
-    public class SortValues{
+public class SortValuesInAscendingOrder {
         public static void main(String args[]) {
             List<Integer> myList = Arrays.asList(10,15,8,49,25,98,98,32,15);
 
